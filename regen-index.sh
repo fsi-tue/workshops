0226e176f0f0e793208b5b5fcd1830680807aaae
Jules Kreuer <contact@juleskreuer.eu>
2022-03-02 20:07:23 +0100

    remove debug print

--- regen-index.sh
+++ regen-index.sh
@@ -10,7 +10,6 @@ echo "" > index.md
 echo -e "# Index" > index.md
 
 for year in $allYears; do
-    echo "$year"
     echo -e "\n## $year\n" >> index.md
     git ls-files -z $year | tr '\0' '\n' | grep -E "\.(pdf|html)$" | sed -E "s/$year\/(.*)/- [\1]($year\/\1)/" >> index.md
 done
