69982c66071b4c9b1f4f41115b5427849a11087f
Jules Kreuer <contact@juleskreuer.eu>
2022-03-02 19:41:37 +0100

    improve regen-index to support many years

--- regen-index.sh
+++ regen-index.sh
@@ -3,10 +3,14 @@
 set -o errexit
 set -o nounset
 
+allYears="2019 2021 2022"
+
 cd $(dirname $0)
+echo "" > index.md
+echo -e "# Index" > index.md
 
-echo -e "# Index\n" > index.md
-
-echo -e "## 2019\n" >> index.md
-
-git ls-files -z 2019 | tr '\0' '\n' | grep -E "\.(pdf|html)$" | sed -E "s/2019\/(.*)/- [\1](2019\/\1)/" >> index.md
+for year in $allYears; do
+    echo "$year"
+    echo -e "\n## $year\n" >> index.md
+    git ls-files -z $year | tr '\0' '\n' | grep -E "\.(pdf|html)$" | sed -E "s/$year\/(.*)/- [\1]($year\/\1)/" >> index.md
+done
